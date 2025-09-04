934e0fa427508d114aff1db63d2cf5577dd7020e
Madhu hasan <113763698+Maadhu16@users.noreply.github.com>
2025-09-04 11:20:16 +0530

    Create Variables.java

---
+++ Intro/Variables.java
@@ -0,0 +1,14 @@
+// Variables are containers for storing data values.
+
+//In Java, create a variable by specifying its Datatype, giving it a name, and optionally assigning a value.
+
+class variables{
+    public static void main(String args[])
+    {
+        int a = 10;                                // int => Datatype, a => Variable name, 10 => value
+        String name = "Maadhu";                    //String => Datatype, name => Variable name, Maadhu => value
+        System.out.println(a);
+        System.out.println(name);
+    }
+
+}
