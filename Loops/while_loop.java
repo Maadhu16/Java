b3802a3fe5dbc4aa3236c4b0bfbabf0fecdbd90b
Madhu hasan <113763698+Maadhu16@users.noreply.github.com>
2025-09-04 16:35:31 +0530

    Create while_loop.java

---
+++ Loops/while_loop.java
@@ -0,0 +1,38 @@
+// While loop => execute a block of code as long as a specified condition is true
+// example:
+
+class while_loop{
+    public static void main(String args[])
+    {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i+=1;
+        }
+    }
+}
+
+Output => 1
+          2
+          3
+          4
+          5
+
+//example2:
+
+  class while_loop{
+    public static void main(String args[])
+    {
+        int i = 2;
+        while (i <= 10){
+            System.out.println(i);
+            i+=2;
+        }
+    }
+}
+
+Output => 2
+          4
+          6
+          8
+          10
