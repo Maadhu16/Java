8dd594c839ac01f70b0c9b2ed46d07e60d4085ac
Madhu hasan <113763698+Maadhu16@users.noreply.github.com>
2025-09-04 15:34:13 +0530

    Create if & else statement.java

---
+++ conditional statement/if & else statement.java
@@ -0,0 +1,35 @@
+// if => code runs if condition is true
+// example:
+
+class if_else{
+    public static void main(String args[])
+    {
+        int a = 10;
+        if (a == 10)
+        {
+            System.out.print("yes");
+        }
+    }
+}
+
+Output => yes
+
+// else => code runs if condition is false
+// example:
+
+class if_else{
+    public static void main(String args[])
+    {
+        int a = 10;
+        if (a == 5)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+    }
+}
+
+Output => no
