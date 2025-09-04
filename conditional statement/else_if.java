b7c86bc58eed37f451244387fff6c83b6a0f8546
Madhu hasan <113763698+Maadhu16@users.noreply.github.com>
2025-09-04 15:40:53 +0530

    Create else_if.java

---
+++ conditional statement/else_if.java
@@ -0,0 +1,21 @@
+// else if => used to check more than one condition.
+//example:
+
+class else_if{
+    public static void main(String args[])
+    {
+        int marks = 75;
+
+        if (marks >= 90) {
+            System.out.println("Grade A");
+        } else if (marks >= 75) {
+            System.out.println("Grade B");
+        } else if (marks >= 50) {
+            System.out.println("Grade C");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
+
+Output => Grade B
