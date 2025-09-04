5d1b9dfd60248d5bd03e5847a0875c29c1667e3d
Madhu hasan <113763698+Maadhu16@users.noreply.github.com>
2025-09-04 11:08:16 +0530

    Create basic syntax.java

---
+++ Intro/basic syntax.java
@@ -0,0 +1,8 @@
+// Java program has a class name which must match the filename, and that every program must contain the main() method.
+
+class hello{
+    public static void main(String args[])
+    {
+       System.out.println("Hello");     // Print syntax
+    }
+}
