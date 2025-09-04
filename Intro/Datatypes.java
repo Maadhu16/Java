f71b4f062ebb57ae2af7b14d36e2fa38e0100ad1
Madhu hasan <113763698+Maadhu16@users.noreply.github.com>
2025-09-04 15:00:36 +0530

    Create Datatypes.java

---
+++ Intro/Datatypes.java
@@ -0,0 +1,23 @@
+// Primitive data types:
+
+class Datatypes{
+    public static void main(String args[])
+    {
+        int a = 25;                   // int => for whole numbers
+        double b = 15.25152;          // double => for decimal numbers(double precision)
+        float c = 15.5f;              // float => for decimal numbers(single precision)
+        boolean d = True;             // boolean => for true or false conditions
+        char e = 'A';                 // char => for single characters
+    }
+}
+
+// non-primitive data types:
+
+class Datatypes{
+    public static void main(String args[])
+    {
+        String name = "Maadhu"          // String => for text 
+        int[] numbers = {1, 2, 3};      // Arrays [] => Store multiple values of type int
+        String[] students = {"maadhu","roshan","vasanth"}   // Arrays [] => Store multiple values of type String
+    }
+}
